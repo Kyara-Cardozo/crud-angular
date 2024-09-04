cf296dac058d9ef5fa6d7d1c0a5c4134512b2a28
Kyara Cardozo <69278952+Kyara-Cardozo@users.noreply.github.com>
2024-09-04 19:28:41 -0300

    interface

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatTableModule, MatCardModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
+
+  courses: any [] = [];
+
+  constructor() {}
+
+  ngOnInit(): void{
+
+  }
 
 }
