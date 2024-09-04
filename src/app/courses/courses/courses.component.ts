29dbd6b8d56cfddd822b56aac091dda6d17e9119
Kyara Cardozo <69278952+Kyara-Cardozo@users.noreply.github.com>
2024-09-04 19:37:38 -0300

    Tabela

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { Course } from '.././model/course';
 
 @Component({
   selector: 'app-courses',
@@ -10,14 +11,16 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
 
-  courses: any [] = [];
+  courses: Course [] = [{ _id:"1", name:"Curso Angular", category:"Front-end"}];
+  displayedColumns: string[] = ['name','category'];
+  dataSource = [];
 
-  constructor() {}
+  constructor() {
 
+  }
   ngOnInit(): void{
 
   }
-
 }
