4181719e20513acbf86bca867b56c56ca3975faa
Kyara Cardozo <69278952+Kyara-Cardozo@users.noreply.github.com>
2024-09-05 18:25:29 -0300

    tratamento de erro

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -15,7 +15,7 @@ export class CoursesService {
   return this.httpClient.get<Course[]>(this.API)
     .pipe(
       first(),
-      delay(1000000),
+      delay(1000),
       tap(courses => console.log(courses))
     );
   }
